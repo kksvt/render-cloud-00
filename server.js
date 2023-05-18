bcce9b64dc2d596793d25b5643f276f571456ae7
Krzysztof <krzysztof.swiatek@student.uj.edu.pl>
2023-05-18 15:23:22 +0200

    Added the profile resource

--- server.js
+++ server.js
@@ -23,6 +23,34 @@ app.get('/add_user', function(req, res) {
     users.addUser(req.query.last_name, req.query.first_name);
     res.end("User added");
 })
+/*
+app.get('/profile', function(req, res) {
+    console.log(req.body);
+    console.log(req.query);
+    users.getUsers((err, data) => {
+        if (req.query['user'] === undefined || req.query['user'] === null || req.query['user'].length == 0) {
+            res.end('No username specified.');
+            return;
+        }
+        const use_last_name = req.query['last_name'] !== undefined && 
+            req.query['last_name'] !== null && 
+            req.query['last_name'].length > 0;
+        const user = data.find(
+            (u) => u.first_name === req.query['user'] &&
+             (!use_last_name || u.last_name == req.query['last_name'])
+            );
+        if (user === undefined || user === null) {
+            res.end('Invalid username.');
+            return;
+        }
+        res.end(user.first_name + ' ' + user.last_name);
+    });
+});
+*/
+
+app.get('/profile', function(req, res) {
+    res.sendFile('public/profile.html', {root: __dirname });
+});
 
 const PORT = process.env.PORT || 8080;
 
